bf8568dd12c1aada0ef0504434973d843591cd43
Jinbo Chi <chijinbo@gmail.com>
2022-03-05 17:37:04 +1300

    Make password reset use a secure hash to create reset token Update password reset process

--- plugins/Login/PasswordResetter.php
+++ plugins/Login/PasswordResetter.php
@@ -169,12 +169,12 @@ class PasswordResetter
 
         $login = $user['login'];
 
-        $keySuffix = time() . Common::getRandomString($length = 32);
-        $this->savePasswordResetInfo($login, $newPassword, $keySuffix);
+        $resetToken = $this->generatePasswordResetToken($user);
+        $this->savePasswordResetInfo($login, $newPassword, $resetToken);
 
         // ... send email with confirmation link
         try {
-            $this->sendEmailConfirmationLink($user, $keySuffix);
+            $this->sendEmailConfirmationLink($user, $resetToken);
         } catch (Exception $ex) {
             // remove password reset info
             $this->removePasswordResetInfo($login);
@@ -195,8 +195,8 @@ class PasswordResetter
         $resetInfo = $this->getPasswordToResetTo($login);
         if ($resetInfo === false
             || empty($resetInfo['hash'])
-            || empty($resetInfo['keySuffix'])
-            || !$this->isTokenValid($resetToken, $user, $resetInfo['keySuffix'])
+            || empty($resetInfo['resetToken'])
+            || !$this->isTokenValid($resetToken, $user, $resetInfo['resetToken'])
         ) {
             throw new Exception(Piwik::translate('Login_InvalidOrExpiredToken'));
         }
@@ -241,18 +241,20 @@ class PasswordResetter
      *
      * @param string $token The reset token to check.
      * @param array $user The user information returned by the UsersManager API.
-     * @param string $keySuffix The suffix used in generating a token.
+     * @param string $resetToken The reset token in db
      * @return bool true if valid, false otherwise.
      */
-    public function isTokenValid($token, $user, $keySuffix)
+    public function isTokenValid($token, $user, $resetToken)
     {
         $now = time();
 
         // token valid for 24 hrs (give or take, due to the coarse granularity in our strftime format string)
-        for ($i = 0; $i <= 24; $i++) {
-            $generatedToken = $this->generatePasswordResetToken($user, $keySuffix, $now + $i * 60 * 60);
-            if ($generatedToken === $token) {
-                return true;
+        if($token === $resetToken){
+            for ($i = 0; $i <= 24; $i++) {
+                $expiry = strftime('%Y%m%d%H', $now + $i * 60 * 60);
+                if ($this->passwordHelper->verify($expiry . $user['login'] . $user['email'] . $user['ts_password_modified'] . $user['password'],$resetToken)) {
+                    return true;
+                }
             }
         }
 
@@ -266,26 +268,22 @@ class PasswordResetter
      * The reset token is generated using a user's email, login and the time when the token expires.
      *
      * @param array $user The user information.
-     * @param string $keySuffix The suffix used in generating a token.
      * @param int|null $expiryTimestamp The expiration timestamp to use or null to generate one from
      *                                  the current timestamp.
      * @return string The generated token.
      */
-    public function generatePasswordResetToken($user, $keySuffix, $expiryTimestamp = null)
+    public function generatePasswordResetToken($user, $expiryTimestamp = null)
     {
         /*
-         * Piwik does not store the generated password reset token.
-         * This avoids a database schema change and SQL queries to store, retrieve, and purge (expired) tokens.
+         * The generated password reset token will be saved in option table
+         *
          */
         if (!$expiryTimestamp) {
             $expiryTimestamp = $this->getDefaultExpiryTime();
         }
 
         $expiry = strftime('%Y%m%d%H', $expiryTimestamp);
-        $token = $this->generateSecureHash(
-            $expiry . $user['login'] . $user['email'] . $user['ts_password_modified'] . $keySuffix,
-            $user['password']
-        );
+        $token =  $this->passwordHelper->hash($expiry . $user['login'] . $user['email'] . $user['ts_password_modified'] . $user['password']);
         return $token;
     }
 
@@ -293,60 +291,6 @@ class PasswordResetter
     {
         $passwordPlain = UsersManager::getPasswordHash($passwordPlain);
         return $this->passwordHelper->verify($passwordPlain, $passwordHash);
-    }
-
-    /**
-     * Generates a hash using a hash "identifier" and some data to hash. The hash identifier is
-     * a string that differentiates the hash in some way.
-     *
-     * We can't get the identifier back from a hash but we can tell if a hash is the hash for
-     * a specific identifier by computing a hash for the identifier and comparing with the
-     * first hash.
-     *
-     * @param string $hashIdentifier A unique string that identifies the hash in some way, can,
-     *                               for example, be user information or can contain an expiration date,
-     *                               or whatever.
-     * @param string $data Any data that needs to be hashed securely, ie, a password.
-     * @return string The hash string.
-     */
-    protected function generateSecureHash($hashIdentifier, $data)
-    {
-        // mitigate rainbow table attack
-        $halfDataLen = strlen($data) / 2;
-
-        $stringToHash = $hashIdentifier
-                      . substr($data, 0, $halfDataLen)
-                      . $this->getSalt()
-                      . substr($data, $halfDataLen)
-                      ;
-
-        return $this->hashData($stringToHash);
-    }
-
-    /**
-     * Returns the string salt to use when generating a secure hash. Defaults to the value of
-     * the `[General] salt` INI config option.
-     *
-     * Derived classes can override this to provide a different salt.
-     *
-     * @return string
-     */
-    protected function getSalt()
-    {
-        return SettingsPiwik::getSalt();
-    }
-
-    /**
-     * Hashes a string.
-     *
-     * Derived classes can override this to provide a different hashing implementation.
-     *
-     * @param string $data The data to hash.
-     * @return string
-     */
-    protected function hashData($data)
-    {
-        return Common::hash($data);
     }
 
     /**
@@ -420,15 +364,12 @@ class PasswordResetter
      * Sends email confirmation link for a password reset request.
      *
      * @param array $user User info for the requested password reset.
-     * @param string $keySuffix The suffix used in generating a token.
+     * @param string $resetToken The token suffix
      */
-    private function sendEmailConfirmationLink($user, $keySuffix)
+    private function sendEmailConfirmationLink($user, $resetToken)
     {
         $login = $user['login'];
         $email = $user['email'];
-
-        // construct a password reset token from user information
-        $resetToken = $this->generatePasswordResetToken($user, $keySuffix);
 
         $confirmPasswordModule = $this->confirmPasswordModule;
         $confirmPasswordAction = $this->confirmPasswordAction;
@@ -456,11 +397,11 @@ class PasswordResetter
      *
      * @param string $login The user login for whom a password change was requested.
      * @param string $newPassword The new password to set.
-     * @param string $keySuffix The suffix used in generating a token.
+     * @param string $resetToken The hashed token used in the url
      *
      * @throws Exception if a password reset was already requested within one hour
      */
-    private function savePasswordResetInfo($login, $newPassword, $keySuffix)
+    private function savePasswordResetInfo($login, $newPassword, $resetToken)
     {
         $optionName = self::getPasswordResetInfoOptionName($login);
 
@@ -485,7 +426,7 @@ class PasswordResetter
 
         $optionData = [
             'hash' => $this->passwordHelper->hash(UsersManager::getPasswordHash($newPassword)),
-            'keySuffix' => $keySuffix,
+            'resetToken' => $resetToken,
             'timestamp' => $time,
             'requests' => $count+1
         ];
